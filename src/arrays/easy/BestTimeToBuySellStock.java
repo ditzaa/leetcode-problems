dfc428edb7c5ae83ec36678e02bfde51f1b5c660
Mitu Adrian <adrianmitu83@gmail.com>
2025-01-08 17:23:54 +0200

    Arrays: Best Time to Buy and Sell Stock

---
+++ src/arrays/easy/BestTimeToBuySellStock.java
@@ -0,0 +1,22 @@
+package arrays.easy;
+
+public class BestTimeToBuySellStock {
+    // complexity = O(n)
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int lowestPrice = prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < lowestPrice) {
+                lowestPrice = prices[i];
+            }
+            int profitIfSoldToday = prices[i] - lowestPrice;
+
+            if (profit < profitIfSoldToday) {
+                profit = profitIfSoldToday;
+            }
+        }
+
+        return profit;
+    }
+}
